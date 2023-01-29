a3bbcd807e423b4cb1a8c2503a19b146cf05440b
YakubBaciev <ibragim.boss1993@mail.ru>
2023-01-29 21:36:33 +0300

    Pizza presto bon appetito

---
+++ src/components/Categories.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
+  const onClickCategorie = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, index) => (
+          <li
+            key={index}
+            onClick={() => onClickCategorie(index)}
+            className={activeIndex === index ? "active" : ""}
+          >
+            {value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
