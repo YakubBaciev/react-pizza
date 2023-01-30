711d026447f8907e287f08bb4db0c9dd67fe8e8a
YakubBaciev <ibragim.boss1993@mail.ru>
2023-01-30 20:36:36 +0300

    fetch запрос на mockApi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import React from "react";
+
 import "./scss/app.scss";
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
 
-import pizzas from "./assets/pizzas.json";
-
 function App() {
+  const [items, setAtems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://639c95a242e3ad6927364e55.mockapi.io/items")
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setAtems(json);
+      });
+  }, []);
+  console.log(items);
   return (
     <div className="wrapper">
       <Header />
@@ -18,7 +30,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
+            {items.map((obj) => (
               <PizzaBlock key={obj.id} {...obj} />
             ))}
           </div>
