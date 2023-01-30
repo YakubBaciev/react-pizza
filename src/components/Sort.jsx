d28ff44ca26092be4d7cf055ce633478110fc632
YakubBaciev <ibragim.boss1993@mail.ru>
2023-01-30 14:01:56 +0300

    активировал сортировку

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,4 +1,15 @@
+import React from "react";
+
 function Sort() {
+  const [open, setOpen] = React.useState(false);
+  const [activeSort, setActiveSort] = React.useState(0);
+  const sortList = ["популярности", "цене", "алфавиту"];
+
+  const onClickSort = (index) => {
+    setActiveSort(index);
+    setOpen(false);
+  };
+
   return (
     <div className="sort">
       <div className="sort__label">
@@ -15,15 +26,23 @@ function Sort() {
           />
         </svg>
         <b>Сортировка по:</b>
-        <span>популярности</span>
+        <span onClick={() => setOpen(!open)}>{sortList[activeSort]}</span>
       </div>
-      <div className="sort__popup">
-        <ul>
-          <li className="active">популярности</li>
-          <li>цене</li>
-          <li>алфавиту</li>
-        </ul>
-      </div>
+      {open && (
+        <div className="sort__popup">
+          <ul>
+            {sortList.map((name, index) => (
+              <li
+                onClick={() => onClickSort(index)}
+                key={index}
+                className={activeSort === index ? "active" : ""}
+              >
+                {name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
